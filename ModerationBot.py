df86ea332d45a40cdceebbfcfb2480e76d5919bf
yassin <yassin.ouchmih@student.hogent.be>
2025-04-13 15:01:42 +0200

    testen van moderation met hardgecodeerde bad words volgende stap ai implementeren voor pattern matching

--- ModerationBot.py
+++ ModerationBot.py
@@ -8,7 +8,8 @@ load_dotenv()
 token = os.getenv("Discord_ModeratorBot_Token")
 
 client = discord.Client(intents=discord.Intents.all())
-block_words = ["badword1", "badword2", "badword3","http://, https://"] 
+client = commands.Bot(command_prefix='>', intents=discord.Intents.all())
+block_words = ["badword1", "badword2", "badword3"] 
 
 @client.event
 async def on_ready():
@@ -16,14 +17,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
-  if message.author != client.user:
-    for word in block_words:
-      if "Moderator" not in str(message.author.roles) and word in str(message.content.tolower()):
-       await message.delete()
-       await message.channel.send(f"Bericht van {message.author.mention} is verwijderd omdat het ongepast was.")
-       return
-    print("Bericht niet verwijderd")        
+    if message.author == client.user:
+        return
+    
+    # Check for moderator role
+    is_moderator = any(role.name == "Moderator" for role in message.author.roles)
+    
+    if not is_moderator:
+        message_content = message.content.lower()
+        
+        # Check for blocked words
+        if any(word in message_content for word in block_words):
+            await message.delete()
+            await message.channel.send(f"Bericht van {message.author.mention} is verwijderd omdat het ongepast was.")
+            return
+        
+        # Check for URLs (simple version)
+        if "http://" in message_content or "https://" in message_content:
+            await message.delete()
+            await message.channel.send(f"Bericht van {message.author.mention} is verwijderd omdat het links bevatte.")
+            return
+    
+    print("Bericht niet verwijderd")
+    
+"""
+client.command(name='ban')
+@commands.has_role("Moderator")
+async def ban_user(ctx, member: discord.member, *, reason="Geen reden opgegeven"):
+"""
 client.run(token)
 
 
