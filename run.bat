3714deb5067f5af9bb332840b24bc6998a0cb1a7
yassin <yassin.ouchmih@student.hogent.be>
2025-06-09 14:03:59 +0200

    run bat aangepast

--- run.bat
+++ run.bat
@@ -2,3 +2,5 @@
 call .venv\Scripts\activate
 start "ModerationBot" cmd /k python ModerationBot.py
 start "WelcomeBot" cmd /k python WelcomeBot.py
+start "AiBot" cmd /k python AiBot.py
+start "SocialsBot" cmd /k python SocialsBot.py
