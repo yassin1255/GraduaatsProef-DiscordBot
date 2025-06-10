9b1a31c37f32e533d6add1993514b879a24a63ee
yassin <yassin.ouchmih@student.hogent.be>
2025-06-10 19:40:07 +0200

    remove welcome bot

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 @echo off
 call .venv\Scripts\activate
 start "ModerationBot" cmd /k python ModerationBot.py
-start "WelcomeBot" cmd /k python WelcomeBot.py
 start "AiBot" cmd /k python AiBot.py
 start "SocialsBot" cmd /k python SocialsBot.py
